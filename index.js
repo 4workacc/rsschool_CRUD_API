98b779077084236e7dc2ae2214a59f164af82724
Eugen Svirbutovich <4workacc@gmail.com>
2023-01-11 10:42:02 +0300

    feat: some crud

--- index.js
+++ index.js
@@ -4,58 +4,63 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const server = http.createServer((req, res) => {
-    let apiPath = req.url.substring(1, req.url.length);
-    console.log(apiPath);
+    const validAPI = '/api/users';
     switch (req.method) {
         case 'GET':
-            if (apiPath.split('/').length === 2 && apiPath === 'api/users') {
+            if (req.url === validAPI) {
                 console.log('GET api/users');
+                res.statusCode = 200;
+                res.end();
             }
-            else if (apiPath.split('/').length === 3  && apiPath === 'api/users') {
-                console.log(`GET api info about user ${apiPath.split('/')[2]}`);
+            else if (req.url.indexOf(`${validAPI}/`) !== -1) {
+                let requestUserId = req.url.split('/')[3];
+                console.log(`GET api info about user ${requestUserId}`);
+                res.statusCode = 200;
+                res.end();
             }
             else {
-                res.statusCode = 404;      
-                res.end();         
+                res.statusCode = 404; //400
+                res.end();
                 console.log('GET route error');
             }
             break;
         case 'POST':
-            switch (apiPath) {
-                case 'api/users':
-                    console.log('POST api/users');
-                    break;
-                default:
-                    res.statusCode = 404;
-                    res.end();
-                    console.log('POST route error');
-            };
+            if (req.url === validAPI) {
+                console.log('POST api/users');
+                res.statusCode = 201;
+                res.end();
+            }
+            else {
+                res.statusCode = 400;
+                res.end();
+                console.log('POST route error');
+            }
             break;
         case 'PUT':
-            switch (apiPath) {
-                case 'api/users/{userId}':
-                    console.log('PUT api/users/{userId}');
-                    break;
-                default:
-                    res.statusCode = 404;
-                    res.end();
-                    console.log('PUT route error');
-            };
+            if (req.url.indexOf(`${validAPI}/`) !== -1) {
+                console.log('PUT api/users/{userId}');
+                res.statusCode = 200;
+                res.end();
+            } else {
+                res.statusCode = 404; //400
+                res.end();
+                console.log('PUT route error');
+            }
             break;
         case 'DELETE':
-            switch (apiPath) {
-                case 'api/users/${userId}':
-                    console.log('DELETE api/users/${userId}');
-                    break;
-                default:
-                    res.statusCode = 404;
-                    res.end();
-                    console.log('DELETE route errror');
-            };
+            if (req.url.indexOf(`${validAPI}/`) !== -1) {
+                console.log('DELETE api/users/${userId}');
+                res.statusCode = 204;
+                res.end();
+            } else {
+                res.statusCode = 404; //400
+                res.end();
+                console.log('DELETE route errror');
+            }              
             break;
-        default:           
+        default:
             console.log('never');
-    }   
+    }
 });
 
 server.listen(process.env.PORT, () => {
